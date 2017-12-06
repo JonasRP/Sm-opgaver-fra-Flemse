e1e8c4c20e3c76ae9ecb4056c0bdeadfc20abee0
JonasRP <jonas.paulsen2@gmail.com>
2017-12-06 14:42:11 +0100

    Initial commit

---
+++ src/Hjemmeopgave1.java
@@ -0,0 +1,16 @@
+
+public class Hjemmeopgave1 {
+
+	public static void main(String[] args) {
+	 int[] t = {3, 5, 4, 1, 1, 7, 4, 9, 9, 2, 5}; {
+	 
+	for(int i = 1; i < t.length - 1; i++){
+	    if(t[i-1] < t[i] && t[i+1] < t[i]){
+
+	    
+	    
+	    	}
+	    }
+	}
+}
+}
